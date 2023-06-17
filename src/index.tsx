d627faaf15e242c64dcdb32e51bb140efcbc595e
Burhan Keten <ketenburhan@protonmail.com>
2023-06-17 23:02:16 +0300

    loader

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,20 @@ import { createRoot } from "react-dom/client";
 
 import App from "./App";
 import "./i18n";
+import { Center, Loader, Text } from "@mantine/core";
 
 const container = document.getElementById("root") as HTMLElement;
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <React.Suspense fallback="loading">
+    <React.Suspense
+      fallback={
+        <Center w="100%" h="99vh">
+          <Loader c="red" />
+        </Center>
+      }
+    >
       <App />
     </React.Suspense>
   </React.StrictMode>
