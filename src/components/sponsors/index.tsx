b6b8adbe0af34d11d83380fed1fed9cef1ee73fb
Burhan Keten <ketenburhan@protonmail.com>
2023-06-29 18:11:55 +0300

    fix: sponsors for arkhe

--- src/components/sponsors/index.tsx
+++ src/components/sponsors/index.tsx
@@ -26,20 +26,55 @@ export const Sponsors: React.FC = () => {
   const theme = useMantineTheme();
   const { classes, cx } = useStyles();
   return (
-    <Grid align="center" my="xl" justify="center">
-      <Grid.Col span={6}>
-        <Text className={classes.span}>Ana Destekçi</Text>
-        <Image
-          src="/images/univechain.jpeg"
-          height={120}
-          fit="contain"
-          sx={{ backgroundColor: "#f7f7f7" }}
-        />
-      </Grid.Col>
-      <Grid.Col span={6}>
-        <Text className={classes.span}>Medya Sponsoru</Text>
-        <Image src="/images/btchaber.jpeg" height={120} />
-      </Grid.Col>
-    </Grid>
+    <>
+      <Grid align="center" my="xl" justify="center">
+        <Grid.Col span={12}>
+          <Text className={classes.span}>Ana Destekçi</Text>
+        </Grid.Col>
+        <Grid.Col span={12} sm={4}>
+          <a href="https://univechain.com/">
+            <Image
+              src="/images/univechain.png"
+              height={120}
+              fit="scale-down"
+              title="UniveChain"
+            />
+          </a>
+        </Grid.Col>
+        <Grid.Col span={12}>
+          <Text className={classes.span}>Destekçiler</Text>
+        </Grid.Col>
+        <Grid.Col span={12} sm={4}>
+          <a
+            href="https://istanbulblockchainwomen.org/"
+            title="Blockchain Women İstanbul"
+          >
+            <Image
+              src="/images/blockchain_women_istanbul.png"
+              height={80}
+              fit="contain"
+            />
+          </a>
+        </Grid.Col>
+        <Grid.Col span={12} sm={4}>
+          <a href="https://www.altcointurk.com/" title="altcointurk">
+            <Image src="/images/altcointurk.png" height={120} />
+          </a>
+        </Grid.Col>
+        <Grid.Col span={12} sm={4}>
+          <a href="https://tim.com.tr/" title="Tim Danışmanlık">
+            <Image src="/images/tim_logo_tr.svg" height={120} />
+          </a>
+        </Grid.Col>
+        <Grid.Col span={12}>
+          <Text className={classes.span}>Media Sponsoru</Text>
+        </Grid.Col>
+        <Grid.Col span={12} sm={3}>
+          <a href="https://www.btchaber.com/">
+            <Image src="/images/btchaber.png" height={120} fit="scale-down" />
+          </a>
+        </Grid.Col>
+      </Grid>
+    </>
   );
 };
